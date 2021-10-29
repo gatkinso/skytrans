03d0163070eadeb2e842291e8b5c4746f1bc828f
Geoffrey Atkinson <geoffrey.j.atkinson@gmail.com>
2021-10-29 08:05:09 -0400

    work work work

--- bin/pytrans/model.py
+++ bin/pytrans/model.py
@@ -4,32 +4,38 @@ from neomodel.properties import BooleanProperty
 
 class Signal(StructuredRel):
     signal = IntegerProperty(unique_index=False, required=True)
+    global_seq_num = IntegerProperty(unique_index=False, required=False)
 
 class Fork(StructuredRel):
     fork_pid = IntegerProperty(unique_index=False, required=False)
+    global_seq_num = IntegerProperty(unique_index=False, required=False)
 
 class Exec(StructuredRel):
     pid = IntegerProperty(unique_index=False)
     ppid = IntegerProperty(unique_index=False)
+    global_seq_num = IntegerProperty(unique_index=False, required=False)
 
 class SuspendResume(StructuredRel):
     pid = IntegerProperty(unique_index=False)
+    global_seq_num = IntegerProperty(unique_index=False, required=False)
 
 class GetTask(StructuredRel):
     tgt_pid = IntegerProperty(unique_index=False)
+    global_seq_num = IntegerProperty(unique_index=False, required=False)
 
-#class SendPointEvent(StructuredNode):
-#    hostname = StringProperty(unique_index=False, required=True)
-#    global_sequence_number = IntegerProperty(unique_index=False)
-#    es_message_version = IntegerProperty(unique_index=False)
+class Mount(StructuredRel):
+    global_seq_num = IntegerProperty(unique_index=False, required=False)
+    frm = StringProperty(unique_index=False, required=True)
+
+class Unmount(StructuredRel):
+    global_seq_num = IntegerProperty(unique_index=False, required=False)
+    frm = StringProperty(unique_index=False, required=True)
 
 class Process(StructuredNode):
     hostname = StringProperty(unique_index=False, required=True)
     es_message_version = IntegerProperty(unique_index=False)
-
     executable_path = StringProperty(unique_index=False, required=True)
     pid = IntegerProperty(unique_index=False)
-
     ppid = IntegerProperty(unique_index=False)
     original_ppid = IntegerProperty(unique_index=False)
     is_platform_binary = BooleanProperty(unique_index=False)
@@ -46,17 +52,19 @@ class Process(StructuredNode):
 class FileOp(StructuredRel):
     op = StringProperty(unique_index=False, required=True)
     unix_time = IntegerProperty(unique_index=False, required=True)
+    global_seq_num = IntegerProperty(unique_index=False, required=False)
 
 class KextLoad(StructuredRel):
     actor = RelationshipFrom(Process, "LOADED")
+    global_seq_num = IntegerProperty(unique_index=False, required=False)
 
 class KextUnload(StructuredRel):
     actor = RelationshipFrom(Process, "UNLOADED")
+    global_seq_num = IntegerProperty(unique_index=False, required=False)
 
 class File(StructuredNode):
     hostname = StringProperty(unique_index=False, required=True)
     es_message_version = IntegerProperty(unique_index=False)
-
     target_path = StringProperty(unique_index=False, required=True)
 
     creator = RelationshipFrom(Process, "CREATED", model = FileOp)
@@ -66,21 +74,11 @@ class File(StructuredNode):
     closer = RelationshipFrom(Process, "CLOSED", model = FileOp)
     kext_load = RelationshipFrom(Process, "KEXT_LOAD", model = KextLoad)
     kext_unload = RelationshipFrom(Process, "KEXT_UNLOAD", model = KextUnload)
+    mount = RelationshipFrom(Process, "MOUNTED", model = Mount)
+    unmount = RelationshipFrom(Process, "UNMOUNTED", model = Unmount)
 
-class FileMount(StructuredNode):
-    hostname = StringProperty(unique_index=False, required=True)
-    global_sequence_number = IntegerProperty(unique_index=False)
-    es_message_version = IntegerProperty(unique_index=False)
 
-    dir = StringProperty(unique_index=False, required=True)
-    mounted = RelationshipFrom("FileMount", "MOUNTED")
-    actor = RelationshipFrom(Process, "MOUNT")
-
-class FileUnmount(StructuredNode):
-    hostname = StringProperty(unique_index=False, required=True)
-    global_sequence_number = IntegerProperty(unique_index=False)
-    es_message_version = IntegerProperty(unique_index=False)
-
-    dir = StringProperty(unique_index=False, required=True)
-    mounted = RelationshipTo(FileMount, "UNMOUNTED")
-    actor = RelationshipFrom(Process, "UNMOUNT")
+#class SendPointEvent(StructuredNode):
+#    hostname = StringProperty(unique_index=False, required=True)
+#    global_sequence_number = IntegerProperty(unique_index=False)
+#    es_message_version = IntegerProperty(unique_index=False)
--- bin/pytrans/pytrans_server.py
+++ bin/pytrans/pytrans_server.py
@@ -2,6 +2,7 @@ from concurrent import futures
 from logging import FileHandler
 from timeit import default_timer as timer
 from google.protobuf.json_format import MessageToJson
+from google.protobuf.any_pb2 import Any
 
 from neo4j import GraphDatabase
 from neomodel import config
@@ -20,19 +21,6 @@ from estypes import es_event_type_t, es_proc_suspend_resume_type_t
 
 class Neo4jClient:
     def __init__(self):
-        self.notify_exec = True
-        self.notify_create = True
-        self.notify_close = True
-        self.notify_open = True
-        self.notify_signal = True
-        self.notify_get_task = True
-        self.notify_fork = True
-        self.notify_mount = True
-        self.notify_unmount = True
-        self.notify_suspend_resume = True
-        self.notify_kextload = True
-        self.notify_kext_unload = True
-
         self.processed = False
 
     def create_process(self, stencil_, hostname_, es_message_version_, executable_path_,
@@ -82,8 +70,9 @@ class Neo4jClient:
 
         event_type = stencil_.int_values["event_type"]
         es_message_version = stencil_.int_values["es_message->version"]
+        gsn = stencil_.int_values["global_seq_num"]
 
-        if self.notify_exec and event_type == es_event_type_t.ES_EVENT_TYPE_NOTIFY_EXEC:
+        if event_type == es_event_type_t.ES_EVENT_TYPE_NOTIFY_EXEC:
             proc = self.do_proc_create(stencil_, hn = hn, ev_ver = es_message_version)
             exec_proc = self.create_process(stencil_,
                                     hostname_ = hn,
@@ -95,10 +84,27 @@ class Neo4jClient:
                                     is_platform_binary_ = "exec.target->is_platform_binary",
                                     is_es_client_ = "exec.target->is_es_client").save()
 
-            proc.exec.connect(exec_proc, {'pid': stencil_.int_values["exec.target->pid"], 'ppid': stencil_.int_values["exec.target->ppid"]})
+            proc.exec.connect(exec_proc, {'pid': stencil_.int_values["exec.target->pid"], 
+                        'ppid': stencil_.int_values["exec.target->ppid"], 'global_seq_num': gsn})
             return processed
 
-        if self.notify_create and event_type == es_event_type_t.ES_EVENT_TYPE_NOTIFY_CREATE:
+        if event_type == es_event_type_t.ES_EVENT_TYPE_NOTIFY_SIGNAL:
+            proc = self.do_proc_create(stencil_, hn = hn, ev_ver = es_message_version)
+            sig_proc = self.create_process(stencil_,
+                                    hostname_ = hn,
+                                    es_message_version_ = es_message_version,
+                                    executable_path_ = "signal.target->executable->path", 
+                                    pid_ = "signal.target->pid",
+                                    ppid_ = "signal.target->ppid",
+                                    original_ppid_ = "signal.target->original_ppid",
+                                    is_platform_binary_ = "signal.target->is_platform_binary",
+                                    is_es_client_ = "signal.target->is_es_client").save()
+
+            sig = stencil_.int_values["signal.sig"]
+            proc.signal.connect(sig_proc, {'signal': sig, 'global_seq_num': gsn})
+            return processed
+
+        if event_type == es_event_type_t.ES_EVENT_TYPE_NOTIFY_CREATE:
             proc = self.do_proc_create(stencil_, hn = hn, ev_ver = es_message_version)
             if "create.destination.existing_file->path" in stencil_.string_values:
                 tgt = stencil_.string_values["create.destination.existing_file->path"]
@@ -116,60 +122,46 @@ class Neo4jClient:
             rel = tgt_file.creator.relationship(proc)
 
             if rel == None:
-                tgt_file.creator.connect(proc, {'op': 'CREATED', 'unix_time' : unix_time })
+                tgt_file.creator.connect(proc, {'op': 'CREATED', 'unix_time' : unix_time, 'global_seq_num': gsn })
             else:
                 rel.unix_time = unix_time
             return processed
 
-        if self.notify_close and event_type == es_event_type_t.ES_EVENT_TYPE_NOTIFY_CLOSE:
+        if event_type == es_event_type_t.ES_EVENT_TYPE_NOTIFY_CLOSE:
+            proc = self.do_proc_create(stencil_, hn = hn, ev_ver = es_message_version)
             unix_time = int(stencil_.string_values["Unix Time"])
 
             tgt_file = self.create_file(stencil_,
                                         hn,
                                         es_message_version_ = es_message_version,
-                                        target_path_ = "close.target->path").save()
+                                        target_path_ = stencil_.string_values["close.target->path"]).save()
 
             rel = tgt_file.closer.relationship(proc)
 
             if rel == None:
-                tgt_file.closer.connect(proc, {'op': 'CLOSED', 'unix_time' : unix_time })
+                tgt_file.closer.connect(proc, {'op': 'CLOSED', 'unix_time' : unix_time, 'global_seq_num': gsn })
             else:
                 rel.unix_time = unix_time
             return processed
 
-        if self.notify_open and event_type == es_event_type_t.ES_EVENT_TYPE_NOTIFY_OPEN:
+        if event_type == es_event_type_t.ES_EVENT_TYPE_NOTIFY_OPEN:
+            proc = self.do_proc_create(stencil_, hn = hn, ev_ver = es_message_version)
             unix_time = int(stencil_.string_values["Unix Time"])
 
             tgt_file = self.create_file(stencil_,
                                         hn,
                                         es_message_version_ = es_message_version,
-                                        target_path_ = "open.file->path").save()
+                                        target_path_ = stencil_.string_values["open.file->path"]).save()
 
             rel = tgt_file.opener.relationship(proc)
 
             if rel == None:
-                tgt_file.opener.connect(proc, {'op': 'OPENED', 'unix_time' : unix_time })
+                tgt_file.opener.connect(proc, {'op': 'OPENED', 'unix_time' : unix_time, 'global_seq_num': gsn })
             else:
                 rel.unix_time = unix_time
-            return
-
-        if self.notify_signal and event_type == es_event_type_t.ES_EVENT_TYPE_NOTIFY_SIGNAL:
-            proc = self.do_proc_create(stencil_, hn = hn, ev_ver = es_message_version)
-            sig_proc = self.create_process(stencil_,
-                                    hostname_ = hn,
-                                    es_message_version_ = es_message_version,
-                                    executable_path_ = "signal.target->executable->path", 
-                                    pid_ = "signal.target->pid",
-                                    ppid_ = "signal.target->ppid",
-                                    original_ppid_ = "signal.target->original_ppid",
-                                    is_platform_binary_ = "signal.target->is_platform_binary",
-                                    is_es_client_ = "signal.target->is_es_client").save()
-
-            sig = stencil_.int_values["signal.sig"]
-            proc.signal.connect(sig_proc, {'signal': sig})
             return processed
 
-        if self.notify_get_task and event_type == es_event_type_t.ES_EVENT_TYPE_NOTIFY_GET_TASK:
+        if event_type == es_event_type_t.ES_EVENT_TYPE_NOTIFY_GET_TASK:
             proc = self.do_proc_create(stencil_, hn = hn, ev_ver = es_message_version)
             tgt_proc = self.create_process(stencil_,
                                     hostname_ = hn,
@@ -182,10 +174,10 @@ class Neo4jClient:
                                     is_es_client_ = "get_task.target->is_es_client").save()
 
             tgt_pid = stencil_.int_values["get_task.target->pid"]
-            proc.gettask.connect(tgt_proc, {'tgt_pid': tgt_pid})
+            proc.gettask.connect(tgt_proc, {'tgt_pid': tgt_pid, 'global_seq_num': gsn})
             return processed
 
-        if self.notify_fork and event_type == es_event_type_t.ES_EVENT_TYPE_NOTIFY_FORK:
+        if event_type == es_event_type_t.ES_EVENT_TYPE_NOTIFY_FORK:
             proc = self.do_proc_create(stencil_, hn = hn, ev_ver = es_message_version)
             child_proc = self.create_process(stencil_,
                                     hostname_ = hn,
@@ -197,44 +189,48 @@ class Neo4jClient:
                                     is_platform_binary_ = "fork.child->is_platform_binary",
                                     is_es_client_ = "fork.child->is_es_client").save()
 
-            proc.fork.connect(child_proc, {'fork_pid': stencil_.int_values["fork.child->pid"]})
+            proc.fork.connect(child_proc, {'fork_pid': stencil_.int_values["fork.child->pid"], 'global_seq_num': gsn})
             return processed
 
-        if self.notify_mount and event_type == es_event_type_t.ES_EVENT_TYPE_NOTIFY_MOUNT:
+        if event_type == es_event_type_t.ES_EVENT_TYPE_NOTIFY_MOUNT:
             proc = self.do_proc_create(stencil_, hn = hn, ev_ver = es_message_version)
-            m_from = stencil_.string_values["mount.statfs->f_mntfromname"]
-            m_to = stencil_.string_values["mount.statfs->f_mntonname"]
 
-            mnt_from = model.FileMount.nodes.first_or_none(hostname = hn, dir = m_from)
-            if mnt_from == None:
-                mnt_from = model.FileMount(hostname = hn, dir = m_from).save()
+            tgt_file = self.create_file(stencil_,
+                                        hn,
+                                        es_message_version_ = es_message_version,
+                                        target_path_ = stencil_.string_values["mount.statfs->f_mntonname"]).save()
 
-            mnt_to = model.FileMount.nodes.first_or_none(hostname = hn, dir = m_to)
-            if mnt_to == None:
-                mnt_to = model.FileMount(hostname = hn, dir = m_to).save()
+            rel = tgt_file.mount.relationship(proc)
 
-            mnt_to.mounted.connect(mnt_from)
-            mnt_from.actor.connect(proc)
+            frm = stencil_.string_values["mount.statfs->f_mntfromname"]
+            if rel == None:
+
+                tgt_file.mount.connect(proc, {'global_seq_num': gsn, 'frm' : frm })
+            else:
+                rel.global_seq_num = gsn
+
             return processed
 
-        if self.notify_unmount and event_type == es_event_type_t.ES_EVENT_TYPE_NOTIFY_UNMOUNT:
+        if event_type == es_event_type_t.ES_EVENT_TYPE_NOTIFY_UNMOUNT:
             proc = self.do_proc_create(stencil_, hn = hn, ev_ver = es_message_version)
-            m_from = stencil_.string_values["unmount.statfs->f_mntfromname"]
-            m_to = stencil_.string_values["unmount.statfs->f_mntonname"]
+            # = int(stencil_.string_values["Unix Time"])
 
-            mnt_from = model.FileMount.nodes.first_or_none(hostname = hn, dir = m_from)
-            if mnt_from == None:
-                mnt_from = model.FileMount(hostname = hn, dir = m_from).save()
+            tgt_file = self.create_file(stencil_,
+                                        hn,
+                                        es_message_version_ = es_message_version,
+                                        target_path_ = stencil_.string_values["unmount.statfs->f_mntonname"]).save()
 
-            mnt_to = model.FileUnmount.nodes.first_or_none(hostname = hn, dir = m_to)
-            if mnt_to == None:
-                mnt_to = model.FileUnmount(hostname = hn, dir = m_to).save()
+            rel = tgt_file.unmount.relationship(proc)
 
-            mnt_to.mounted.connect(mnt_from)
-            mnt_from.actor.connect(proc)
+            frm = stencil_.string_values["unmount.statfs->f_mntfromname"]
+            if rel == None:
+                tgt_file.unmount.connect(proc, {'global_seq_num': gsn, 'frm' : frm })
+            else:
+                rel.global_seq_num = gsn
+
             return processed
 
-        if self.notify_suspend_resume and event_type == es_event_type_t.ES_EVENT_TYPE_NOTIFY_PROC_SUSPEND_RESUME:
+        if event_type == es_event_type_t.ES_EVENT_TYPE_NOTIFY_PROC_SUSPEND_RESUME:
             proc = self.do_proc_create(stencil_, hn = hn, ev_ver = es_message_version)
             sr_pid = stencil_.int_values["proc_suspend_resume.target->pid"]
 
@@ -249,11 +245,11 @@ class Neo4jClient:
                                     is_es_client_ = "proc_suspend_resume.target->is_es_client").save()
  
             if stencil_.int_values["proc_suspend_resume.type"] == es_proc_suspend_resume_type_t.ES_PROC_SUSPEND_RESUME_TYPE_RESUME:
-                proc.resume.connect(child_proc, {'sr_pid': sr_pid})
+                proc.resume.connect(child_proc, {'sr_pid': sr_pid, 'global_seq_num': gsn})
             elif stencil_.int_values["proc_suspend_resume.type"] == es_proc_suspend_resume_type_t.ES_PROC_SUSPEND_RESUME_TYPE_SUSPEND:
-                proc.suspend.connect(child_proc, {'sr_pid': sr_pid})
+                proc.suspend.connect(child_proc, {'sr_pid': sr_pid, 'global_seq_num': gsn})
             elif stencil_.int_values["proc_suspend_resume.type"] == es_proc_suspend_resume_type_t.ES_PROC_SUSPEND_RESUME_TYPE_SHUTDOWN_SOCKETS:
-                proc.socks.connect(child_proc, {'sr_pid': sr_pid})
+                proc.socks.connect(child_proc, {'sr_pid': sr_pid, 'global_seq_num': gsn})
             return processed
 
         processed = False
@@ -262,7 +258,36 @@ class Neo4jClient:
 class TransportServicer(transport_pb2_grpc.TransportServicer):
     """Provides methods that implement functionality of route guide server."""
     def __init__(self):
-        return
+        self.notify_exec = True
+        self.notify_create = True
+        self.notify_close = False
+        self.notify_open = True
+        self.notify_signal = True
+        self.notify_get_task = False
+        self.notify_fork = True
+        self.notify_mount = True
+        self.notify_unmount = True
+        self.notify_suspend_resume = True
+        self.notify_kextload = False
+        self.notify_kext_unload = False
+
+    def DoPathMutes(self, res, stencil, type, path):
+            stencil.string_values[type] = path
+            any_message = Any()
+            any_message.Pack(stencil)
+            res.impl.append(any_message)
+
+    def DoSubs(self, res, stencil, flag, value):
+        if flag == True:
+            stencil.int_values["subscribe"] = value
+            any_message = Any()
+            any_message.Pack(stencil)
+            res.impl.append(any_message)
+        else:
+            stencil.int_values["unsubscribe"] = value
+            any_message = Any()
+            any_message.Pack(stencil)
+            res.impl.append(any_message)
 
     def Exchange(self, request, context):
         hn = request.meta.data.string_values["Hostname"]
@@ -294,6 +319,23 @@ class TransportServicer(transport_pb2_grpc.TransportServicer):
         res.meta.data.int_values["Req id"] = request.meta.data.int_values["Req id"]
         res.meta.data.int_values["Event Count"] = bs
 
+        stencil_ = stencil_pb2.Stencil()
+        self.DoSubs(res, stencil_, self.notify_exec, es_event_type_t.ES_EVENT_TYPE_NOTIFY_EXEC)
+        self.DoSubs(res, stencil_, self.notify_create, es_event_type_t.ES_EVENT_TYPE_NOTIFY_CREATE)
+        self.DoSubs(res, stencil_, self.notify_close, es_event_type_t.ES_EVENT_TYPE_NOTIFY_CLOSE)
+        self.DoSubs(res, stencil_, self.notify_open, es_event_type_t.ES_EVENT_TYPE_NOTIFY_OPEN)
+        self.DoSubs(res, stencil_, self.notify_signal, es_event_type_t.ES_EVENT_TYPE_NOTIFY_SIGNAL)
+        self.DoSubs(res, stencil_, self.notify_get_task, es_event_type_t.ES_EVENT_TYPE_NOTIFY_GET_TASK)
+        self.DoSubs(res, stencil_, self.notify_fork, es_event_type_t.ES_EVENT_TYPE_NOTIFY_FORK)
+        self.DoSubs(res, stencil_, self.notify_mount, es_event_type_t.ES_EVENT_TYPE_NOTIFY_MOUNT)
+        self.DoSubs(res, stencil_, self.notify_unmount, es_event_type_t.ES_EVENT_TYPE_NOTIFY_UNMOUNT)
+        self.DoSubs(res, stencil_, self.notify_suspend_resume, es_event_type_t.ES_EVENT_TYPE_NOTIFY_PROC_SUSPEND_RESUME)
+        #self.DoSubs(res, stencil_, self.notify_kextload, es_event_type_t.ES_EVENT_TYPE_NOTIFY_EXEC)
+        #self.DoSubs(res, stencil_, self.notify_kext_unload, es_event_type_t.ES_EVENT_TYPE_NOTIFY_EXEC)
+
+        mutee = "/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/Metadata.framework/Versions/A/Support/"
+        self.DoPathMutes(res, stencil_, "mute prefix", mutee)
+
         return res
 
 @contextlib.contextmanager
@@ -316,7 +358,6 @@ def run_server(bind_address):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=multiprocessing.cpu_count()), maximum_concurrent_rpcs=10)
     transport_pb2_grpc.add_TransportServicer_to_server(
         TransportServicer(), server)
-    #server.add_insecure_port('[::]:1967')
     server.add_insecure_port(bind_address)
     server.start()
     server.wait_for_termination()
