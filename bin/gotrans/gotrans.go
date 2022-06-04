d8def9e0ba73c832e26850ef6456b5db52f7a7fd
Geoffrey Atkinson <geoffrey_atkinso@hotmail.com>
2022-06-04 11:03:19 -0400

    Initial go commit

--- bin/gotrans/gotrans.go
+++ bin/gotrans/gotrans.go
@@ -9,53 +9,44 @@ import (
 
 	"google.golang.org/grpc"
 
-	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/examples/data"
+	pi "google.golang.org/protobuf/runtime/protoimpl"
 
-	pb "../../skytransproto/com/skyloupe.skytrans/transport"
+	pb "transport"
 )
 
 var (
-	tls      = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
-	certFile = flag.String("cert_file", "", "The TLS cert file")
-	keyFile  = flag.String("key_file", "", "The TLS key file")
-	port     = flag.Int("port", 50051, "The server port")
+	port = flag.Int("port", 1967, "The server port")
 )
 
-type transportServer struct {
+// server is used to implement TransportServer.
+type server struct {
 	pb.UnimplementedTransportServer
 }
 
-func (transportServer) Exchange(context.Context, *pb.Request) (*pb.Response, error) {
-	return &pb.Response{}, nil
+func (s *server) GetString(msg *pb.Request) string {
+	return pi.X.MessageStringOf(msg)
 }
 
-func newServer() *transportServer {
-	s := &transportServer{}
-	return s
+// SayHello implements TransportServer
+func (s *server) Exchange(ctx context.Context, in *pb.Request) (*pb.Response, error) {
+	log.Printf(s.GetString(in))
+	return &pb.Response{}, nil
 }
 
 func main() {
 	flag.Parse()
-	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	var opts []grpc.ServerOption
-	if *tls {
-		if *certFile == "" {
-			*certFile = data.Path("x509/server_cert.pem")
-		}
-		if *keyFile == "" {
-			*keyFile = data.Path("x509/server_key.pem")
-		}
-		creds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
-		if err != nil {
-			log.Fatalf("Failed to generate credentials %v", err)
-		}
-		opts = []grpc.ServerOption{grpc.Creds(creds)}
+
+	myserver := grpc.NewServer()
+	pb.RegisterTransportServer(myserver, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err := myserver.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
 	}
-	grpcServer := grpc.NewServer(opts...)
-	pb.RegisterTransportServer(grpcServer, newServer())
-	grpcServer.Serve(lis)
 }
