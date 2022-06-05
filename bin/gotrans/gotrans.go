cb57beb03a3735da66095cb56f4ad37eb5769493
Geoffrey Atkinson <geoffrey.j.atkinson@gmail.com>
2022-06-05 12:28:35 -0400

    talks to db

--- bin/gotrans/gotrans.go
+++ bin/gotrans/gotrans.go
@@ -9,9 +9,11 @@ import (
 
 	"google.golang.org/grpc"
 
-	pi "google.golang.org/protobuf/runtime/protoimpl"
+	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
 
 	pb "transport"
+
+	pi "google.golang.org/protobuf/runtime/protoimpl"
 )
 
 var (
@@ -27,9 +29,49 @@ func (s *server) GetString(msg *pb.Request) string {
 	return pi.X.MessageStringOf(msg)
 }
 
+func (s *server) DoEvent(msg *pb.Request, uri, username, password string) error {
+	driver, err := neo4j.NewDriver(uri, neo4j.BasicAuth(username, password, ""))
+	if err != nil {
+		log.Printf("NewDriver failed")
+		return err
+	}
+	defer driver.Close()
+
+	session := driver.NewSession(neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite})
+	defer session.Close()
+
+	//"CREATE (a:Greeting) SET a.message = $message RETURN a.message + ', from node ' + id(a)",
+	//map[string]interface{}{"message": "hello, world"})
+
+	var gmt string
+	gmt = msg.Meta.Data.GetStringValues()["GMT Time"]
+	var hostname string
+	hostname = msg.Meta.Data.GetStringValues()["Hostname"]
+	//log.Printf("%s :  %s", gmt, hostname)
+	//log.Printf(pi.X.MessageStringOf(msg.Meta.Data))
+	_, err = session.WriteTransaction(func(transaction neo4j.Transaction) (interface{}, error) {
+		result, err := transaction.Run(
+			"CREATE (p:Process) SET p = {gmt:$gmt, hostname:$hostname}",
+			map[string]interface{}{"gmt": gmt, "hostname": hostname})
+		if err != nil {
+			log.Printf("Run failed")
+			return nil, err
+		}
+
+		return result.Consume()
+	})
+	if err != nil {
+		log.Printf("WriteTransaction failed: %v", err)
+		return err
+	}
+
+	return err
+}
+
 // SayHello implements TransportServer
 func (s *server) Exchange(ctx context.Context, in *pb.Request) (*pb.Response, error) {
-	log.Printf(s.GetString(in))
+	//log.Printf(s.GetString(in))
+	s.DoEvent(in, "neo4j://192.168.135.199:7687", "neo4j", "password")
 	return &pb.Response{}, nil
 }
 
